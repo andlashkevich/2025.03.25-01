09a34d67151bb6677ac0b935de6e3ff4d0d8b6e7
Andrey Lashkevich <andlashkevich@ya.ru>
2025-03-27 02:20:00 +0300

    01

---
+++ index.js
@@ -0,0 +1,44 @@
+const yargs = require("yargs");
+const pkg = require("./package.json");
+const { addNote, printNotes, removeNote } = require("./notes.controller");
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove note by Id",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Not id",
+      demandOption: true,
+    },
+  },
+  async handler({ id }) {
+    removeNote(id);
+  },
+});
+yargs.parse();
